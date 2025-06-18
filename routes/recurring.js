a48393066e693a15c273ac400604c46a30cd44d8
sahil ranadive <109140095+ranadive-25@users.noreply.github.com>
2025-06-19 03:34:54 +0530

    Update recurring.js

--- routes/recurring.js
+++ routes/recurring.js
@@ -6,4 +6,7 @@ router.post('/', recurringController.addRecurringExpense);
 router.get('/', recurringController.getRecurringExpenses);
 router.post('/run', recurringController.runRecurringExpenses);
 
+router.put('/:id', controller.updateRecurringExpense);
+router.delete('/:id', controller.deleteRecurringExpense);
+
 module.exports = router;
